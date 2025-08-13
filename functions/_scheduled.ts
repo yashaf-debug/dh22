63b9edaa244ae78d645524ee9530acb6d08256fc
yashaf-debug <radchenko.yakov@gmail.com>
2025-08-13 20:33:50 +0300

    fix(ci): switch to wrangler pages deploy; remove optional scheduled function

--- functions/_scheduled.ts
+++ functions/_scheduled.ts
@@ -1,4 +1,3 @@
-export const onSchedule = async (event: any, env: any, ctx: any) => {
-  // прогреваем кэш sitemap раз в день
-  await fetch('https://dh22.ru/sitemap.xml', { cf: { cacheTtl: 21600, cacheEverything: true } } as any);
+export const onSchedule: PagesFunction = async (event, env, ctx) => {
+  await fetch('https://dh22.ru/sitemap.xml', { cf: { cacheTtl: 21600, cacheEverything: true } });
 };
